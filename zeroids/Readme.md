c0977cc46a0ae0dba334c6f59c4ed993b8552d8a
provascript <154516973+provascript@users.noreply.github.com>
2024-06-27 09:23:00 +0200

    Add files via upload

---
+++ zeroids/Readme.md
@@ -0,0 +1,30 @@
+Requirements
+- python 2.7
+- pygame 1.9.1
+
+
+basta eseguire il file zeroids.py con python
+
+
+
+- Bottone del mouse - salta intro
+- Spazio - spara
+- n - passa al livello successivo
+- p - metti in pausa il gioco
+
+- assets contiene i file statici (immagini, suoni, ..)
+- core contiene il codice sorgente
+  - sigla contiene il codice e i file dell'intro
+  - colorlog codice per il logging colorato
+  - ufo, turret, asteroids contentono il codice dei nemici
+  - players contiene il codice dell'astronave principale
+  - main.py contiene il loop principale
+  - datastractures contiene il codice per la gestione degli eventi e per l'oggetto globale delle informazioni generali
+  - sprites contiene il codice di base degli sprite e il generatore di sprite
+  - exceptions contiene gli errori personalizzati
+  - gamecontroller contiene il coordinatore delle fasi di gioco e il codice di gestione delle collisioni
+  - texts contiene il generatore di test
+  - weapons contiene il codice di gestione delle armi
+  - sounds contiene il generatore di suoni
+
+
---
+++ zeroids/core/main.py
@@ -0,0 +1,58 @@
+import pygame
+import os
+import intro.sigla as sigla
+
+from datastructures import EventQueue, GameContext
+from gamecontroller import GameController
+import colorlog as log
+
+
+tilemap = dict()
+
+
+def main():
+    resolution = (1200, 800)
+    max_fps = 50
+    default_backcolor = (0, 0, 0)
+    pygame.init()
+
+    screen = pygame.display.set_mode(resolution)
+    clock = pygame.time.Clock()
+
+    filename = os.path.normpath("assets/images/stars.jpg")
+    image = pygame.image.load(filename)
+    background_image = image.subsurface((0,0), (resolution[0], resolution[1])).copy()
+    background_image.convert()
+    background_image.set_alpha(127)
+
+    main_event_queue = EventQueue()
+    clock = pygame.time.Clock()
+    context_data = { "clock": clock,
+                     "resolution": resolution,
+                     "screen": screen,
+                    }
+    game_context = GameContext(context_data)
+    game_controller = GameController(main_event_queue, game_context)
+    loop = True
+    clock.tick(max_fps)
+    clock.tick(max_fps)
+
+    # intro module
+    sigla.intro_title(screen, game_context)
+    pygame.time.wait(500)
+    sigla.fade_to_black(resolution, screen)
+    sigla.personaggi_entrano_in_scena(screen, game_context)
+    sigla.fade_to_black(resolution, screen)
+
+    while loop:
+        loop = main_event_queue.handle_events()
+        background_change, background, screen_elements = game_controller.update()
+        screen.fill(default_backcolor)
+        screen.blit(background_image, (0,0))
+        screen_elements.draw(screen)
+        pygame.display.flip()
+        clock.tick(max_fps)
+#if __name__=='__main__':
+   # main()
+    
+pygame.quit()
